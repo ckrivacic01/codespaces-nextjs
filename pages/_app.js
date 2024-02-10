0a165962d59e9e7daa58a194f11239af1421c702
ckrivacic01 <86799500+ckrivacic01@users.noreply.github.com>
2024-02-10 19:59:03 +0000

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
 import '../global.css'
+import { Provider } from 'react-redux'
+import {store} from './components/app/store.js'
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Provider store={store}>
+  <Component {...pageProps} />
+  <Provider/>
 }
